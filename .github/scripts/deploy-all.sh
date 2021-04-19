218e7e4d19862153f89207bdc8dbf4b29d10de4b
Paul B <paul@bonaud.fr>
2021-04-19 23:39:32 +0200

    publish API documentation on Bump.sh

---
+++ .github/scripts/deploy-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -eu
+
+# Install bump-cli (for now published as a Ruby gem)
+gem install bump-cli
+
+# For each file from the available `*.json` files
+for api in spec/json/twilio*.json; do
+    # Extract the API name from filename `twilio_<api_name>_vN.json`
+    apiName="${api%_*}"
+    apiName="${apiName#*_}"
+
+    # Create documentation <apiName> from the api definition file into a single Bump Hub
+    bump deploy --doc "${apiName}" --token "${BUMP_TOKEN}" --hub twilio --auto-create "${api}"
+done
